63f537f9a5ca3a2b6b894da596fbf7b6e60ee7bc
andri yansyah <andriyansyah58@gmail.com>
2017-05-21 23:40:24 +0700

    tambah data

---
+++ app/src/main/java/com/example/andri/iak_biginer/splashScreen/OrderActivity.java
@@ -0,0 +1,15 @@
+package com.example.andri.iak_biginer.splashScreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andri.iak_biginer.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
